f8a101c7f4511b967c4ce009a5c8ed6bbfd8b53e
Vlad <vlad@mail.ru>
2022-05-09 18:49:42 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict"; /* строгий режим */
+
+/* const answer = [];
+answer[0] = prompt("Ваше имя?", "");
+document.write(answer);
+
+const age = confirm("Вам есть 18?");
+document.write(age);
+
+alert("Спасибо что ответили на наши вопросы. Нажмите ОК чтобы продолжить"); */
+
+/* const answer = [];
+answer[0] = prompt("Ваше имя?", "");
+const age = confirm("Вам есть 18?");
+
+document.write(`Привет ${answer}, Вам 18 и более лет и это ${age}`); */
