389dd8f4bc7da1f800365becce74ccbbc5245370
vlad <vlad@mail.ru>
2022-05-09 20:14:37 +0500

    praktics

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ answer[0] = prompt("Ваше имя?", "");
 const age = confirm("Вам есть 18?");
 
 document.write(`Привет ${answer}, Вам 18 и более лет и это ${age}`); */
+
+let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+let filmsname = prompt("Один из просмотренных фильмов?");
+let filmscif = +prompt("Оценка фильма?", "");
+
+
+let PersonMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+
+};
+
+PersonMovieDB.movies[filmsname] = filmscif;
+
+console.log(PersonMovieDB);
